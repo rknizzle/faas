39ded23bf51cbf10df0f71a849f48af55f2b3cae
rknizzle <ryan@autotiv.com>
2020-01-10 19:45:43 -0500

    Added a function to list all running Docker containers on the system

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
@@ -12,5 +14,23 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(cli)
+
+	// list containers
+	fmt.Println("Listing running containers if any...")
+	listContainers(*cli)
+}
+
+// Lists all running Docker containers on the system
+func listContainers(cli client.Client) error {
+	// Get list of running containers
+	containers, err := cli.ContainerList(context.Background(), types.ContainerListOptions{})
+	if err != nil {
+		return err
+	}
+
+	// Print out all running containers
+	for _, container := range containers {
+		fmt.Printf("%s %s\n", container.ID[:10], container.Image)
+	}
+	return nil
 }
