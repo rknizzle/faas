7cd4789f062fb5298d5939be3e7d7d82320b2acc
rknizzle <ryan@autotiv.com>
2020-07-13 21:02:58 -0400

    add a client package and some init functions
            - im going to add a binary and this functionality will be used
              by the faas init command

---
+++ client/init.go
@@ -0,0 +1,66 @@
+package client
+
+import (
+	"io/ioutil"
+)
+
+func Init() error {
+	err := writeIndexFile()
+	if err != nil {
+		return err
+	}
+	err = writeDockerfile()
+	if err != nil {
+		return err
+	}
+	err = writeYAMLFile()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeIndexFile() error {
+	indexContents := `if (require.main === module) {
+  console.log('HELLO WORLD!')
+}`
+
+	err := ioutil.WriteFile("index.js", []byte(indexContents), 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeDockerfile() error {
+	dockerfileContents := `FROM node:12
+
+# Create app directory
+WORKDIR /usr/src/app
+
+# Install app dependencies
+COPY package*.json ./
+
+RUN npm install
+
+# Bundle app source
+COPY . .
+
+CMD [ "node", "index.js" ]`
+
+	err := ioutil.WriteFile("Dockerfile", []byte(dockerfileContents), 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func writeYAMLFile() error {
+	yamlContents := `language: node`
+
+	err := ioutil.WriteFile("fn.yaml", []byte(yamlContents), 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+}
