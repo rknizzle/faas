329f965241693c7e9846b3be1859ebc5a68aec4d
rknizzle <ryan@autotiv.com>
2020-10-02 15:45:46 -0400

    extracted out logic into more modular functions

--- api/api.go
+++ api/api.go
@@ -49,6 +49,33 @@ func invokeHandler(m *manager.Manager) gin.HandlerFunc {
 	})
 }
 
+func addFunctionHandler(m *manager.Manager) gin.HandlerFunc {
+	return gin.HandlerFunc(func(c *gin.Context) {
+
+		data, err := fnDataFromReq(c)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Creating function...")
+
+		dir, err := dirFromBase64Data(data.Name, data.File)
+		if err != nil {
+			panic(err)
+		}
+
+		tag := data.Name
+		m.BuildImage(dir, tag)
+
+		// remove temporary directory used to build the image
+		os.RemoveAll(dir)
+
+		c.JSON(200, gin.H{
+			"invoke": c.Request.Host + "/functions/" + tag,
+		})
+	})
+}
+
 func fnDataFromReq(c *gin.Context) (FnData, error) {
 	rawData, err := c.GetRawData()
 	if err != nil {
@@ -63,58 +90,42 @@ func fnDataFromReq(c *gin.Context) (FnData, error) {
 	return fnData, nil
 }
 
-func addFunctionHandler(m *manager.Manager) gin.HandlerFunc {
-	return gin.HandlerFunc(func(c *gin.Context) {
+func dirFromBase64Data(fnName string, base64Data string) (string, error) {
+	filename, err := writeDataToZip(fnName, base64Data)
+	if err != nil {
+		return "", err
+	}
 
-		data, err := fnDataFromReq(c)
-		if err != nil {
-			panic(err)
-		}
+	// unzip the file into a directory called the function name
+	dir := fnName
+	_, err = Unzip(filename, dir)
+	if err != nil {
+		return "", err
+	}
 
-		// the input "file" is expected to be base64 encoded
-		// decode the zip file
-		decoder := base64.NewDecoder(base64.StdEncoding, strings.NewReader(data.File))
-		if err != nil {
-			panic(err)
-		}
+	// remove the zip file now that the directory has been extracted
+	err = os.Remove(filename)
+	if err != nil {
+		return "", err
+	}
 
-		fmt.Println("Creating function...")
+	return dir, nil
+}
 
-		os.Mkdir("tmp", 0755)
-		dir := "tmp/" + data.Name
-		// name to be given to the zip file thats written to disk
-		zipFile := dir + ".zip"
+func writeDataToZip(fnName string, fnData string) (string, error) {
+	filename := fnName + ".zip"
 
-		output, err := os.Create(zipFile)
-		if err != nil {
-			panic(err)
-		}
-		// Close output file
-		defer output.Close()
+	zipFile, err := os.Create(filename)
+	if err != nil {
+		return "", err
+	}
+	defer zipFile.Close()
 
-		// write decoded data to zip file
-		io.Copy(output, decoder)
+	decoder := base64.NewDecoder(base64.StdEncoding, strings.NewReader(fnData))
 
-		// unzip the input zip file to extract the directory containing the function code
-		_, err = Unzip(zipFile, dir)
-		if err != nil {
-			panic(err)
-		}
-		// remove the zip file now that the directory has been extracted
-		err = os.Remove(zipFile)
-		if err != nil {
-			fmt.Println("Failed to remove zip file")
-		}
-
-		tag := data.Name
-		m.BuildImage(dir, tag)
-
-		// remove temporary directory used to build the image
-		os.RemoveAll("tmp/")
-		c.JSON(200, gin.H{
-			"invoke": c.Request.Host + "/functions/" + tag,
-		})
-	})
+	// write decoded data to zip file
+	io.Copy(zipFile, decoder)
+	return filename, nil
 }
 
 // Function found at https://golangcode.com/unzip-files-in-go/ (MIT License)
