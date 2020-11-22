bf8804dfbdb6d7f0c4cf3f03d61a961d88489139
rknizzle <ryan@autotiv.com>
2020-11-22 17:15:34 -0500

    add a comment to the ContainerDeployer

--- internal/gateway/deployer/containerDeployer.go
+++ internal/gateway/deployer/containerDeployer.go
@@ -1,5 +1,7 @@
 package deployer
 
+// ContainerDeployer contains all the methods required to turn function code into a container image
+// and then sent to a container registry where it can be pulled for invocation
 type ContainerDeployer interface {
 	PushImage(string) error
 	BuildImage(string, string) error
