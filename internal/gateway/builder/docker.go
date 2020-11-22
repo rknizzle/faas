afa4d6344844d7f2dcdf9d15c673924f7e251509
rknizzle <ryan@autotiv.com>
2020-11-22 13:20:51 -0500

    implement a docker ContainerBuilder

---
+++ internal/gateway/builder/docker.go
@@ -0,0 +1,95 @@
+package builder
+
+import (
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/client"
+	"github.com/docker/docker/pkg/archive"
+	"io/ioutil"
+	"time"
+)
+
+type DockerBuilder struct {
+	cli  *client.Client
+	auth string
+}
+
+func NewDockerBuilder(registryUsername string, registryPassword string) (*DockerBuilder, error) {
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.40"))
+	if err != nil {
+		return nil, err
+	}
+
+	auth, err := generateRegistryAuth(registryUsername, registryPassword)
+	if err != nil {
+		return nil, err
+	}
+
+	return &DockerBuilder{cli, auth}, nil
+}
+
+// Generate the dockerhub credentials from ENV variables
+func generateRegistryAuth(username string, password string) (string, error) {
+	if username == "" || password == "" {
+		return "", errors.New("Missing Dockerhub username or password")
+	}
+
+	authConfig := types.AuthConfig{
+		Username: username,
+		Password: password,
+	}
+	encodedJSON, err := json.Marshal(authConfig)
+	if err != nil {
+		return "", err
+	}
+	authStr := base64.URLEncoding.EncodeToString(encodedJSON)
+	return authStr, nil
+}
+
+// Builds a Docker image
+func (d DockerBuilder) BuildImage(directory string, tag string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(300)*time.Second)
+	defer cancel()
+
+	// Get a tar file from directory
+	dockerfileTarReader, err := archive.TarWithOptions(directory, &archive.TarOptions{})
+	if err != nil {
+		return err
+	}
+
+	resp, err := d.cli.ImageBuild(
+		ctx,
+		dockerfileTarReader,
+		types.ImageBuildOptions{
+			Dockerfile: "Dockerfile",
+			Tags:       []string{tag},
+			NoCache:    true,
+			Remove:     true,
+		})
+	if err != nil {
+		return err
+	}
+
+	// block until the image is finished building
+	_, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Push an image to remote repository
+func (d DockerBuilder) PushImage(image string) error {
+	ctx := context.Background()
+	out, err := d.cli.ImagePush(ctx, image, types.ImagePushOptions{
+		RegistryAuth: d.auth,
+	})
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	return nil
+}
