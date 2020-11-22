8857d500070a8f19f083f110e8b03cc0b6071848
rknizzle <ryan@autotiv.com>
2020-11-22 17:41:13 -0500

    add comments to the function models

--- internal/models/function.go
+++ internal/models/function.go
@@ -1,11 +1,15 @@
 package models
 
+// Function represents a single function that a user deploys and can invoke
 type Function struct {
 	Name  string
 	Image string
 }
 
+// FnData contains the name and the data of a function that a user is deploying. The data is stored
+// as a base64 encoded zip file containing the function code
 type FnData struct {
 	Name string
-	File string // base64 encoded zip file
+	// base64 encoded zip file
+	File string
 }
