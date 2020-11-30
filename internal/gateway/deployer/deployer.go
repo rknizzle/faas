a1a570fe43ef39ff9c78bfbe25face4cc3fb1378
rknizzle <ryan@autotiv.com>
2020-11-30 14:56:17 -0500

    remove the deployer interface from deployer.go
            - also put the extractor and containerDeployer interfaces into
              deployer.go because that is where they are used

--- internal/gateway/deployer/deployer.go
+++ internal/gateway/deployer/deployer.go
@@ -5,25 +5,34 @@ import (
 	"os"
 )
 
+// containerDeployer contains all the methods required to turn function code into a container image
+// and then sent to a container registry where it can be pulled for invocation
+type containerDeployer interface {
+	BuildImage(string, string) error
+	PushImage(string) error
+}
+
+// extractor extracts the directory containing the function code from the input base64 encoded zip
+// file
+type extractor interface {
+	FnDirFromBase64Data(string, string) (string, error)
+}
+
 // Deployer handles deploying new functions that a user submits by unpacking the function data from
 // a users request and then using a ContainerDeployer to build and push a container image for later
 // invocation
-type Deployer interface {
-	Deploy(models.FnData) error
-}
-
-type deployer struct {
-	c ContainerDeployer
-	e Extractor
+type Deployer struct {
+	c containerDeployer
+	e extractor
 }
 
 // NewDeployer initializes a Deployer with a ContainerDeploy for building and pushing images
-func NewDeployer(c ContainerDeployer, e Extractor) deployer {
-	return deployer{c, e}
+func NewDeployer(c containerDeployer, e extractor) Deployer {
+	return Deployer{c, e}
 }
 
 // Deploy unpacks the function code and then builds and pushes a container image
-func (d deployer) Deploy(data models.FnData) error {
+func (d Deployer) Deploy(data models.FnData) error {
 	dir, err := d.e.FnDirFromBase64Data(data.Name, data.File)
 	if err != nil {
 		return err
@@ -31,7 +40,7 @@ func (d deployer) Deploy(data models.FnData) error {
 
 	tag := data.Name
 	d.c.BuildImage(dir, tag)
-	//b.cBuilder.PushImage()
+	// d.c.PushImage()
 
 	// remove temporary directory used to build the image
 	os.RemoveAll(dir)
