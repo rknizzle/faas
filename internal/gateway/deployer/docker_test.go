c6410da2ed9c19ff81cb4f913eb4079baf50f2c1
rknizzle <ryan@autotiv.com>
2020-12-06 13:13:31 -0500

    add mocks for DockerClient and add a test for BuildImage

--- internal/gateway/deployer/docker_test.go
+++ internal/gateway/deployer/docker_test.go
@@ -1,7 +1,11 @@
 package deployer
 
 import (
+	"context"
+	"io"
 	"testing"
+
+	"github.com/docker/docker/api/types"
 )
 
 func TestGenerateRegistryAuth(t *testing.T) {
@@ -28,5 +32,35 @@ func TestGenerateRegistryAuthMissingPassword(t *testing.T) {
 	_, err := generateRegistryAuth("exampleUser", "")
 	if err == nil {
 		t.Fatal("Expected test case to fail due to missing password")
+	}
+}
+
+// mocks for DockerClient
+type fakeIOReadCloser struct{}
+
+func (f fakeIOReadCloser) Read([]byte) (int, error) { return 0, nil }
+func (f fakeIOReadCloser) Close() error             { return nil }
+
+type mockDockerClient struct{}
+
+func (m mockDockerClient) ImageBuild(context.Context, io.Reader, types.ImageBuildOptions) (types.ImageBuildResponse, error) {
+	return types.ImageBuildResponse{}, nil
+}
+
+func (m mockDockerClient) ImagePush(context.Context, string, types.ImagePushOptions) (io.ReadCloser, error) {
+	return fakeIOReadCloser{}, nil
+}
+
+func newMockDockerDeployer() DockerDeployer {
+	dc := mockDockerClient{}
+	return DockerDeployer{dc, "xxx"}
+}
+
+func TestBuildImage(t *testing.T) {
+	readAll = func(io.Reader) ([]byte, error) { return nil, nil }
+	dd := newMockDockerDeployer()
+	err := dd.BuildImage("name", "tag")
+	if err != nil {
+		t.Fatalf("err: %s", err)
 	}
 }
