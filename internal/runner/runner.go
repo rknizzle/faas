189389529e1a088d72a32c0e60e6778034c4310f
rknizzle <ryan@autotiv.com>
2020-12-13 14:04:01 -0500

    add a new Runner struct that uses a ContainerRunner...
            ... to start a container and get its IP then it sends an HTTP
    request to the running container

---
+++ internal/runner/runner.go
@@ -0,0 +1,46 @@
+package runner
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+type Runner struct {
+	CR ContainerRunner
+}
+
+func (r Runner) StartFnContainer(image string) (string, error) {
+	id, err := r.CR.RunContainer(image)
+	if err != nil {
+		return "", err
+	}
+
+	ctx := context.Background()
+	ip, err := r.CR.ContainerIP(ctx, id)
+	if err != nil {
+		return "", err
+	}
+
+	return ip, nil
+}
+
+func (r Runner) SendRequestToContainer(ip string, input string) (string, error) {
+	url := fmt.Sprintf("http://%s:8080/invoke", ip)
+
+	resp, err := http.Post(url, "application/json", nil)
+	if err != nil {
+		return "", err
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return "", err
+	}
+
+	bodyString := string(bodyBytes)
+	return bodyString, nil
+}
