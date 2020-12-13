292df16f28dbc0e4a950da997004e3e02821d333
rknizzle <ryan@autotiv.com>
2020-12-13 14:03:22 -0500

    update ContainerRunner interface to include ContainerIP

--- internal/runner/containerRunner.go
+++ internal/runner/containerRunner.go
@@ -1,8 +1,11 @@
 package runner
 
+import "context"
+
 // ContainerRunner contains all the methods required to handle a function invocation by pulling down
 // a function container image from a remote registry and running the function code in the container
 type ContainerRunner interface {
 	PullImage(string) error
-	RunContainer(string) error
+	RunContainer(string) (string, error)
+	ContainerIP(context.Context, string) (string, error)
 }
