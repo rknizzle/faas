3f6b63bc24e0960b61f8e271cc5d6e9dc5f8bae2
rknizzle <ryan@autotiv.com>
2020-12-13 17:19:23 -0500

    update cli tool to output the response from the fn invocation
            - also correctly log errors

--- client/invoke.go
+++ client/invoke.go
@@ -2,34 +2,51 @@ package client
 
 import (
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 )
 
-func Invoke(function string) error {
+func Invoke(function string) (map[string]string, error) {
 	// send HTTP request to server to invoke function AKA spin
 	// up new docker container and run the code
 	client := &http.Client{}
 	r, err := http.NewRequest("POST", "http://localhost:5555/functions/"+function, nil)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	resp, err := client.Do(r)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	defer resp.Body.Close()
 
 	// get the JSON response
-	var result map[string]string
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return err
+
+	var invokeErr struct {
+		message string
 	}
-	return nil
+	var invokeRes struct {
+		response map[string]string
+	}
+
+	// check status code here and unmarshal into the appropriate struct then return the correct value
+	if resp.StatusCode == 200 {
+		err = json.Unmarshal(body, &invokeRes)
+		if err != nil {
+			return nil, err
+		}
+		return invokeRes.response, nil
+	} else {
+		err = json.Unmarshal(body, &invokeErr)
+		if err != nil {
+			return nil, err
+		}
+		return nil, errors.New(invokeErr.message)
+	}
 }
