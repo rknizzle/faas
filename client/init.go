8a8cb49daa1371309e8ca596b1ae05e106589c40
rknizzle <ryan@autotiv.com>
2020-12-13 14:00:45 -0500

    update the javascript function to start an HTTP server
            - use port 8080
            - transitioning to a system where the function container starts
              an HTTP server and the runner sends an HTTP request to the
    container once its running
            - so include a server.js file on init and update the
              package.json packages and update the DOckerfile to expose port
    8080

--- client/init.go
+++ client/init.go
@@ -5,7 +5,11 @@ import (
 )
 
 func Init() error {
-	err := writeIndexFile()
+	err := writeServerFile()
+	if err != nil {
+		return err
+	}
+	err = writeIndexFile()
 	if err != nil {
 		return err
 	}
@@ -22,6 +26,29 @@ func Init() error {
 		return err
 	}
 	return nil
+}
+
+func writeServerFile() error {
+	serverContents := `const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+app.post('/invoke', (req, res) => {
+	console.log('INVOKE TESTING 123')
+	res.json({my: 'response', hello: 'world!'})
+	app.close()
+})
+
+const port = 8080
+app.listen(port, () => {})`
+
+	err := ioutil.WriteFile("server.js", []byte(serverContents), 0755)
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
 
 func writeIndexFile() error {
@@ -48,7 +75,11 @@ func writePackageFile() error {
   },
   "keywords": [],
   "author": "",
-  "license": "ISC"
+  "license": "ISC",
+  "dependencies": {
+    "body-parser": "^1.19.0",
+    "express": "^4.17.1"
+  }
 }`
 
 	err := ioutil.WriteFile("package.json", []byte(packageContents), 0755)
@@ -72,7 +103,8 @@ RUN npm install
 # Bundle app source
 COPY . .
 
-CMD [ "node", "index.js" ]`
+EXPOSE 8080
+CMD [ "node", "server.js" ]`
 
 	err := ioutil.WriteFile("Dockerfile", []byte(dockerfileContents), 0755)
 	if err != nil {
