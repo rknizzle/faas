0fb5389d260ec2fb01007269308b4e2e6c1735d9
rknizzle <ryan@autotiv.com>
2020-12-14 17:58:18 -0500

    move ContainerRunner interface into runner.go where it is used

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -9,6 +9,14 @@ import (
 	"time"
 )
 
+// ContainerRunner contains all the methods required to handle a function invocation by pulling down
+// a function container image from a remote registry and running the function code in the container
+type ContainerRunner interface {
+	PullImage(string) error
+	RunContainer(string) (string, error)
+	ContainerIP(context.Context, string) (string, error)
+}
+
 type Runner struct {
 	CR ContainerRunner
 }
