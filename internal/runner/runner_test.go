4fe6c070b56d76ba78e2d381ce6c894ad1fcfdc2
rknizzle <ryan@autotiv.com>
2020-12-14 19:19:19 -0500

    add unit test that SendRequestToContainer returns an error
            - when the HTTP request gets an error

--- internal/runner/runner_test.go
+++ internal/runner/runner_test.go
@@ -1,6 +1,7 @@
 package runner
 
 import (
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -29,6 +30,26 @@ func TestSendRequestToContainer(t *testing.T) {
 		_, err := r.SendRequestToContainer("http://1.1.1.1:8080/invoke", input)
 		if err != nil {
 			t.Fatalf("err %s", err)
+		}
+	})
+
+	t.Run("Returns an error when the HTTP request returns an error", func(t *testing.T) {
+
+		input := []byte("input")
+		mockHTTPPoster.On(
+			"Post",
+			mock.Anything,
+			"application/json",
+			mock.Anything,
+		).Return(
+			nil,
+			errors.New("test error"),
+		).Once()
+
+		r := NewRunner(DockerRunner{}, mockHTTPPoster)
+		_, err := r.SendRequestToContainer("http://1.1.1.1:8080/invoke", input)
+		if err == nil {
+			t.Fatal("Expected SendRequestToContainer to throw an error")
 		}
 	})
 }
